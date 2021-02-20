3cb767e95d1ef5b5c60d1579b4a6c1ad7cb8c7f4
Robert Chatterton <robbie.chatterton@gmail.com>
2021-02-20 11:27:11 -0500

    real app

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-<<<<<<< HEAD
 import { Dimensions, StyleSheet, Text, View, Image, Button, SafeAreaView } from 'react-native';
-=======
-import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
->>>>>>> c81af73c7cfea24ee6246e76e03056612f6bb6f6
-
 export default function App() {
   console.log(Dimensions.get('window'));
 
   return (
-<<<<<<< HEAD
+
     <SafeAreaView style={styles.container}>
+      <Header />
       <View style={{
         backgroundColor: 'red',
         width: '100%',
         height: '30%'
       }}></View>
     </SafeAreaView>
-=======
-    <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />
-    </View>
->>>>>>> c81af73c7cfea24ee6246e76e03056612f6bb6f6
   );
 }
 
